bed5401fd5ee0150622d924f872451651a327b67
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-04-18 21:11:35 -0400

    Carrito de compras movido a otra vista

--- src/app/modules/products/pages/products-overview-layout/products-overview-layout.component.ts
+++ src/app/modules/products/pages/products-overview-layout/products-overview-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from 'src/assets/images/image-routes';
 
 @Component({
   selector: 'pag-products-overview-layout',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./products-overview-layout.component.scss']
 })
 export class ProductsOverviewLayoutComponent {
+
+  productsList: any[] = [
+    {
+      image: products.product1,
+      name: 'Correa de tiempo',
+      brandName: 'General motors',
+      sku: 'krogm93746917',
+      quantity: 1,
+      price: 175,
+      currency: 'us$'
+    },
+    {
+      image: products.product1,
+      name: 'Correa de tiempo',
+      brandName: 'General motors',
+      sku: 'krogm93746917',
+      quantity: 1,
+      price: 175,
+      currency: 'us$'
+    },
+    {
+      image: products.product1,
+      name: 'Correa de tiempo',
+      brandName: 'General motors',
+      sku: 'krogm93746917',
+      quantity: 1,
+      price: 175,
+      currency: 'us$'
+    },
+  ]
+
+  prebill: any = {
+    basePrice: 175,
+    delivery: 175,
+    totalPrice: 175,
+    discount: '_ US$',
+    currency: 'US$'
+  }
 
 }
